0a4347c6d33618eec5aad8de9f190f78d00b2b4d
Sergei Sharabai <sharabai.sergei@gmail.com>
2023-12-09 22:25:22 +0100

    Add rabbitmq to backend

--- backend/Controllers/DataController.cs
+++ backend/Controllers/DataController.cs
@@ -1,9 +1,15 @@
 using System;
+using System.Text; 
+using Newtonsoft.Json;
 using Microsoft.AspNetCore.Mvc;
 using dotNet_bakery.Models;
 using dotNet_bakery.Repo;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using MQTTnet;
+using MQTTnet.Client.Options;
+using MQTTnet.Extensions.ManagedClient;
+using Microsoft.Extensions.Logging;
 
 namespace dotNet_bakery.Controller;
 
@@ -12,9 +18,49 @@ namespace dotNet_bakery.Controller;
 public class DataController: ControllerBase{
 
     private readonly DataRepository _dataRepository;
+    private IManagedMqttClient? _mqttClient;
+    private readonly ILogger _logger;
 
-    public DataController(DataRepository dataRepository){
+    public DataController(DataRepository dataRepository, ILogger<DataController> logger){
         _dataRepository = dataRepository;
+        _logger = logger;
+        _logger.LogInformation("DataController constructor");
+        ConfigureMqttClient();
+    }
+
+    private void ConfigureMqttClient(){
+        var options = new ManagedMqttClientOptionsBuilder()
+            .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
+            .WithClientOptions(new MqttClientOptionsBuilder()
+                .WithClientId("backend")
+                .WithTcpServer("rabbitmq", 1883)
+                .Build())
+            .Build();
+
+        _mqttClient = new MqttFactory().CreateManagedMqttClient();
+        _mqttClient.SubscribeAsync(new MqttTopicFilterBuilder().WithTopic("sensor/data").Build()).ContinueWith(task => {
+            if (task.IsFaulted)
+                _logger.LogError($"Failed to subscribe to topic: {task.Exception}");
+            else
+                _logger.LogInformation("Successfully subscribed to topic");
+        });
+        _mqttClient.UseApplicationMessageReceivedHandler(e =>
+        {
+            var message = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+            _logger.LogInformation($"Received message: {message}"); 
+            DataModel dataModel = JsonConvert.DeserializeObject<DataModel>(message);
+            HandleReceivedData(dataModel);
+        });
+        _mqttClient.StartAsync(options).ContinueWith(task => {
+            if (task.IsFaulted)
+                _logger.LogError($"Failed to connect to RabbitMQ: {task.Exception}");
+            else
+                _logger.LogInformation("Successfully connected to RabbitMQ");
+        });
+    }
+
+    private async void HandleReceivedData(DataModel dataModel){
+        await _dataRepository.CreateAsync(dataModel);
     }
 
     [HttpGet]
@@ -22,16 +68,9 @@ public class DataController: ControllerBase{
         return await _dataRepository.GetAsync();
     }
 
-    [HttpPost]
-    public async Task<IActionResult> Post([FromBody] DataModel dataModel){
-        await _dataRepository.CreateAsync(dataModel);
-        return CreatedAtAction(nameof(Get), new {id = dataModel.id}, dataModel);
-    }
-
     [HttpDelete("{id}")]
     public async Task<IActionResult> Delete(ObjectId dataId){
         await _dataRepository.DeleteAsync(dataId);
         return NoContent();
     }
-
 }
--- data-generator/data-generator.js
+++ data-generator/data-generator.js
@@ -13,18 +13,18 @@ client.on('connect', function () {
   console.log('Starting data generation every second...');
   client.subscribe([topic], () => {
     console.log(`Subscribe to topic '${topic}'`)
-    client.publish(topic, 'nodejs mqtt test', { qos: 0, retain: false }, (error) => {
-      if (error) {
-        console.error(error)
-      }
-    })
+    // client.publish(topic, 'nodejs mqtt test', { qos: 0, retain: false }, (error) => {
+    //   if (error) {
+    //     console.error(error)
+    //   }
+    // })
   })
   setInterval(generateData, timesPerMinute * 1000);
 });
 
-client.on('message', (topic, payload) => {
-    console.log('Received Message:', topic, payload.toString())
-  })
+// client.on('message', (topic, payload) => {
+//     console.log('Received Message:', topic, payload.toString())
+//   })
 
 client.on('error', function (err) {
   console.error('An error occurred:', err);
